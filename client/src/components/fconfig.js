b2a96662d6d6d23853590ba3c38e8202c351316d
sharon moses J <smj.vit@gmail.com>
2020-07-06 00:10:45 +0530

    scrapper back

--- client/src/components/fconfig.js
+++
@@ -1,9 +0,0 @@
-
-  export var config = {
-    apiKey: "AIzaSyC5eS1E1SPk-jk3AzfZYLe2JWA_1hqWq9I",
-    authDomain: "taskf-aa42f.firebaseapp.com",
-    databaseURL: "https://taskf-aa42f.firebaseio.com",
-    projectId: "taskf-aa42f",
-    storageBucket: "taskf-aa42f.appspot.com",
-    messagingSenderId: "1057613950813"
-  };
